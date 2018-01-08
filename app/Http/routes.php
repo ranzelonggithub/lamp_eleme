2db8349fb5bdb7efbd2e89c731a1acab0724e897
donglingjiu <18301390328@163.com>
2018-01-08 10:53:07 +0800

    route

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -14,3 +14,9 @@
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/test1', function () {
+    return view('test1');
+});
+Route::get('/test2', function () {
+    return view('test2');
+});
